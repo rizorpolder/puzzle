d0a54c59bea5849caab24631229e0cd03d087af9
Kirill Knyazev <k.knyazev@bgf.games>
2024-07-13 23:55:46 +0300

    fix unused

--- Assets/Scripts/Field/GameFieldManager.cs
+++ Assets/Scripts/Field/GameFieldManager.cs
@@ -1,84 +1,82 @@
-using System;
 using System.Collections.Generic;
 using Data;
 using Field;
-using Global;
 using UnityEngine;
-using UnityEngine.UI;
 
-public class GameFieldManager : MonoBehaviour
+namespace Managers
 {
-	[SerializeField] private Vector2 FieldSize;
-	[SerializeField] private List<PuzzleCell> pool;
-
-
-	private FieldData _data;
-
-	private void Awake()
+	public class GameFieldManager : MonoBehaviour
 	{
-		GenerateField();
-		InitializeField();
-		// доступ к саб спрайтам получают через загрузку ресурсов
-		// заранее кэшировать данные в конфиг и дергать оттуда
-	}
-	
-	private void GenerateField()
-	{
-		//заранее загруженные пользовательские данные  или созданные с нуля
-		_data = new FieldData();
-	}
+		private string tempArtName = "art_template";
 
-	private void InitializeField()
-	{
-		for (int i = 0;  i < FieldSize.x; i++)
+		[SerializeField] private PuzzleCell prefab;
+		[SerializeField] private Transform fieldRoot;
+		[SerializeField] private List<PuzzleCell> pool;
+
+		private FieldData _data;
+
+		private void Awake()
 		{
-			for (int j = 0; j < FieldSize.y; j++)
-			{
-				pool[i+j].Initialize(i,j);
-				var textureUnits = ConfigurableRoot.Instance.ImageRepositoryConfig.GetSprites("art_template");
-			}
+			GenerateField();
+			InitializeField();
+			// доступ к саб спрайтам получают через загрузку ресурсов
+			// заранее кэшировать данные в конфиг и дергать оттуда
 		}
-	}
 
-	private void LoadField()
-	{
-
-	}
-
-	private void FillField()
-	{
-		//Fill elements from index to index
-		//Check if disabled - skip
-	}
-
-	private void RenameElements()
-	{
-		int currentIndex = 0;
-		for (int i = 0;  i < FieldSize.x; i++)
+		private void GenerateField()
 		{
-			for (int j = 0; j < FieldSize.y; j++)
-			{
-				var element = pool[currentIndex];
-				element.gameObject.name = $"{i}x{j}_cell";
-				currentIndex++;
-			}
+			//заранее загруженные пользовательские данные  или созданные с нуля
+			_data = new FieldData();
 		}
-	}
 
-	[UnityEditor.CustomEditor(typeof(GameFieldManager))]
-	public class GameFieldManagerEditor : UnityEditor.Editor
-	{
-		public override void OnInspectorGUI()
+		private void InitializeField()
 		{
-			var _target = target as GameFieldManager;
-			var style = new GUIStyle(GUI.skin.button);
-			style.normal.textColor = new Color(0, 150f / 255f, 90f / 255f);
-			if (GUILayout.Button("Create field", style, GUILayout.Width(180), GUILayout.Height(20)))
-			{
-				_target.RenameElements();
-			}
-
-			base.OnInspectorGUI();
 		}
+
+		private void LoadField()
+		{
+		}
+
+		private void FillField()
+		{
+			//Fill elements from index to index
+			//Check if disabled - skip
+		}
+
+#if UNITY_EDITOR
+
+		// [UnityEditor.CustomEditor(typeof(GameFieldManager))]
+		// public class GameFieldManagerEditor : UnityEditor.Editor
+		// {
+		// 	private GameFieldManager _target;
+		//
+		// 	public override void OnInspectorGUI()
+		// 	{
+		// 		_target = target as GameFieldManager;
+		// 		var style = new GUIStyle(GUI.skin.button);
+		// 		style.normal.textColor = new Color(0, 150f / 255f, 90f / 255f);
+		// 		if (GUILayout.Button("Create field", style, GUILayout.Width(180), GUILayout.Height(20)))
+		// 		{
+		// 			RenameElements();
+		// 		}
+		//
+		// 		base.OnInspectorGUI();
+		// 	}
+		//
+		// 	private void RenameElements()
+		// 	{
+		// 		int currentIndex = 0;
+		// 		for (int i = 0; i < FieldSize.x; i++)
+		// 		{
+		// 			for (int j = 0; j < FieldSize.y; j++)
+		// 			{
+		// 				var element = _target.pool[currentIndex];
+		// 				element.gameObject.name = $"{i}x{j}_cell";
+		// 				currentIndex++;
+		// 			}
+		// 		}
+		// 	}
+		// }
+#endif
 	}
 }
