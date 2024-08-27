1399b20a33f9cfada02a3300d14fa5099f0f8392
Kirill Knyazev <k.knyazev@bgf.games>
2024-08-27 09:44:02 +0300

    add base level windows and items

---
+++ Assets/Scripts/UI/Windows/LevelWindow/LevelInfoWindow.cs
@@ -0,0 +1,8 @@
+using UI.Common;
+
+namespace UI.Windows.LevelWindow
+{
+	public class LevelInfoWindow : BaseWindow
+	{
+	}
+}
---
+++ Assets/Scripts/UI/Windows/LevelWindow/LevelsWindow.cs
@@ -0,0 +1,13 @@
+using UI.Common;
+
+namespace UI.Windows.LevelWindow
+{
+	public class LevelsWindow : BaseWindow
+	{
+
+		///2 вьюхи (категории и уровни)
+		/// игрок клацая по категории проваливается во вторую вьюху где уже уровни со звездами
+		/// звезды из правого угла вынести
+
+	}
+}
